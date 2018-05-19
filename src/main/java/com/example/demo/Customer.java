d228d1a94d5dd9eca61e101c2f1308029616c379
mehtamu29 <mumehta@gmail.com>
2018-05-19 19:25:07 +0100

    Sai1

--- src/main/java/com/example/demo/Customer.java
+++ src/main/java/com/example/demo/Customer.java
@@ -1,4 +1,21 @@
 package com.example.demo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
+    private Long id;
+    @NotNull
+    private String first;
+    @NotNull
+    private String second;
+
+    @NotNull
+    private String email;
 }
--- src/test/java/com/example/demo/CustomerTests.java
+++ src/test/java/com/example/demo/CustomerTests.java
@@ -1,13 +1,46 @@
 package com.example.demo;
 
+import org.assertj.core.api.BDDAssertions;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import java.util.Set;
 
 public class CustomerTests {
+
+
+    private Validator validator;
+
+    @Before
+    public void before() throws Throwable {
+        LocalValidatorFactoryBean localValidatorFactoryBean  = new LocalValidatorFactoryBean();
+        localValidatorFactoryBean.afterPropertiesSet();
+        validator = localValidatorFactoryBean.getValidator();
+
+    }
+
     @Test
-    public void testNewCurtomerHasId(){
-        Customer customer = new Customer();
-        assertNotNull(customer);
+    public void newInsWithValidValuesShouldReturnARecord(){
+        Customer customer = new Customer(1L, "first", "second", "mumehta@gmail.com");
+        assertEquals(customer.getId(), Long.valueOf(1L));
+        assertEquals(customer.getFirst(), "first");
+        //hamcrest
+        assertThat(customer.getFirst(), Matchers.is("first"));
+        //asserttj
+        BDDAssertions.then(customer.getFirst()).isEqualToIgnoringCase("first");
+        BDDAssertions.then(customer.getId()).isNotNull();
+    }
+    @Test
+    public void newInsWithInvalidConstraintsShouldProduceContraintViolations(){
+        Customer customer = new Customer(null,null,null,null);
+        Set<ConstraintViolation<Customer>> violations = validator.validate(customer);
+        BDDAssertions.then(violations.size()).isEqualTo(3);
     }
 }
